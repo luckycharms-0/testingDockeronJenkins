e1b4b355d9d7831aca086fe94b93d90a05330b7f
Lucky Singh <lsingh@bglcorp.com.au>
2022-06-27 23:03:43 +1000

    fix test

--- cypress/e2e/common/google.js
+++ cypress/e2e/common/google.js
@@ -2,6 +2,5 @@ import { Given, When, Then } from "cypress-cucumber-preprocessor/steps"
 
 Then('Go to Google', function() {
  cy.visit("www.google.com")
- cy.get('.gNO89b').should('contain.value', 'Test')
 
 })
